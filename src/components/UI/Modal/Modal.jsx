ae937f633a38ef18a4528ac1c5edb39cf8836124
Aaron MV <epsilon11101@gmail.com>
2023-04-13 17:08:26 -0600

    feat: create component

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 import classes from "./Modal.module.css";
 
-class Modal extends Component {}
+class Modal extends Component {
+  render() {
+    return (
+      <div class="overlay">
+        <div class="modal">
+          <img src="" alt="" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Modal;
