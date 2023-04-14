32daf04f502d4f0fa6e227f10db488d6eb57a28c
Aaron MV <epsilon11101@gmail.com>
2023-04-14 17:51:07 -0600

    feat: finish func and render component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,45 @@
 import React, { Component } from "react";
-
+import api from "../../services/api";
 import classes from "./Searchbar.module.css";
 
 class Searchbar extends Component {
+  state = {
+    data: "",
+  };
+
+  onInputChange = (e) => {
+    this.setState({
+      data: e.target.value,
+    });
+  };
+
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    this.props.inputData(this.state.data);
+    this.restore();
+  };
+
+  restore = () => {
+    this.setState({ data: "" });
+  };
+
   render() {
+    const { data } = this.state;
     return (
       <header className={classes.searchbar}>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={this.onSubmitHandler}>
           <button type="submit" className={classes.button}>
-            <span className={classes["button-label"]}>buscar</span>
+            <span className={classes["button-label"]}>Search</span>
           </button>
 
           <input
+            value={data}
             className={classes.input}
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
+            onChange={this.onInputChange}
           />
         </form>
       </header>
