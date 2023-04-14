b652945643cef9780a828fa7c969a82cc4242d5f
Aaron MV <epsilon11101@gmail.com>
2023-04-14 17:50:51 -0600

    feat: finish component style and reder

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -4,11 +4,18 @@ import classes from "./ImageGalleryItem.module.css";
 
 class ImageGalleryItem extends Component {
   render() {
-    return (
-      <li class="gallery-item">
-        <img src="" alt="" />
-      </li>
-    );
+    const { fetchData } = this.props;
+    return fetchData.map((img) => {
+      return (
+        <img
+          key={img.id}
+          src={img.webformatURL}
+          alt={img.tags}
+          data-sorce={img.largeImageURL}
+          loading="lazy"
+        />
+      );
+    });
   }
 }
 
