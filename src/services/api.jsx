90dad154afda05796167c42cf4fd5b3bc7488ca3
Aaron MV <epsilon11101@gmail.com>
2023-04-14 17:53:43 -0600

    feat: added api class

---
+++ src/services/api.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+class API {
+  constructor() {
+    this.KEY = import.meta.env.VITE_PIXKEY;
+    this.URL = `https://pixabay.com/api/?key=${this.KEY}`;
+
+    this.options = {
+      method: "get",
+      url: this.URL,
+      params: {
+        q: "yellow+flower",
+        image_type: "photo",
+        category: "animals",
+        page: "1",
+        per_page: "100",
+      },
+    };
+  }
+
+  fetch = async () => {
+    try {
+      const response = await axios(this.options);
+      const data = await response.data;
+      return data.hits;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  set query(query) {
+    this.options.params.q = String(query);
+  }
+
+  set page(pageNum) {
+    const number = +pageNum;
+    this.options.params.page = isNaN(number) ? "1" : String(number);
+  }
+}
+const api = new API();
+export default api;
